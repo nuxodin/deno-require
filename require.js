1926e545d9b9e789f4fd57bf596cbef41b6f09a7
Tobias Buschor <tobias.buschor@shwups.ch>
2020-05-09 18:06:09 +0200

    Update require.js

--- require.js
+++ require.js
@@ -4,12 +4,20 @@ import { createRequire } from "https://deno.land/std@v0.42.0/node/module.ts";
  * @global
  * @param {string} path - required module
  */
-globalThis.require = function(path) {
+export function require(path) {
     const e = new Error();
     const calledFile = e.stack.split("\n")[2].replace(/^ +at /, '').replace(/:[0-9]+:[0-9]+/,'');
     const requireFunction = createRequire(calledFile);
     return requireFunction(path);
 }
+
+/**
+ * makes the require function global
+ */
+export function globalize(){
+    globalThis.require = require;
+}
+
 
 
 // todo: test if this works
